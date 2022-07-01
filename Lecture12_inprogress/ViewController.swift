83dfd46d7a513f001788996d118450b8a46871fd
anadzebniauri <ana.dzebniauri@space.ge>
2022-07-01 23:06:23 +0400

    colors

--- Lecture12_inprogress/ViewController.swift
+++ Lecture12_inprogress/ViewController.swift
@@ -11,10 +11,29 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        print("ANA")
 
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func appBackground(_ sender: Any) {
+        let uiColor: [UIColor] = [.red, .orange, .green]
+        self.view.backgroundColor = uiColor.randomElement() ?? .black
+        if labelColor.textColor == view.backgroundColor {
+            print("Same Color")
+        }
+    }
+    
+    
+    @IBAction func labelBackground(_ sender: Any) {
+        let uiColor: [UIColor] = [.red, .orange, .green]
+        labelColor.textColor = uiColor.randomElement() ?? .black
+        if labelColor.textColor == view.backgroundColor {
+            print("Same Color")
+        }
+    }
+    
+    
+    @IBOutlet weak var labelColor: UILabel!
 }
+
 
